1ce85b137b742f9bf8f8fa51d56741b1a63bbbdf
pkh0106 <rudgml742@gmail.com>
2024-11-05 18:16:33 +0900

    ✨Feat. 클릭 이벤트 구현 #29

--- FE/src/widgets/header/UI/Logo.tsx
+++ FE/src/widgets/header/UI/Logo.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import logoImage from '../../../shared/images/BuBu.png';
 
 const Logo = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
-    <button className="flex items-center text-display-bold-32">
+    <button className="flex items-center text-display-bold-32" onClick={handleClick}>
       <img src={logoImage} alt="BuBu" width="34rem" />
       BooBit
     </button>
--- FE/src/widgets/header/index.tsx
+++ FE/src/widgets/header/index.tsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom';
 import Logo from './UI/Logo';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isLogin, setIsLogIn] = useState(true);
+
+  const handleLogout = () => {
+    alert('로그아웃하기');
+  };
+
   return (
     <header className="flex justify-between items-center w-full h-20 px-[16vw] bg-primary text-text-light ">
       <Logo />
-      <div className="flex gap-[1rem]">
-        <Link to="/signin">로그인</Link>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/mypage">마이페이지</Link>
-        <button>로그아웃</button>
-      </div>
+      {isLogin ? (
+        <div className="flex gap-[1rem]">
+          <Link to="/mypage">마이페이지</Link>
+          <button onClick={handleLogout}>로그아웃</button>
+        </div>
+      ) : (
+        <div className="flex gap-[1rem]">
+          <Link to="/signin">로그인</Link>
+          <Link to="/signup">회원가입</Link>
+        </div>
+      )}
     </header>
   );
 };
