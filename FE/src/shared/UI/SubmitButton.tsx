90c6e92f7bd5372df1b57a27520abc554a82912e
pkh0106 <rudgml742@gmail.com>
2024-11-06 17:48:25 +0900

    ✨Feat. SubmitButton 컴포넌트 구현 #5

---
+++ FE/src/shared/UI/SubmitButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface SubmitButtonProps {
+  content: string;
+  onClick: () => void;
+}
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({ content, onClick }) => (
+  <button
+    className="w-[100%] h-[4rem] rounded-[5px] text-display-bold-24 text-text-light bg-accent"
+    onClick={onClick}
+  >
+    {content}
+  </button>
+);
+
+export default SubmitButton;
