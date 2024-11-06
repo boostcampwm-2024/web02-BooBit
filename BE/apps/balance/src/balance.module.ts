1038f8ccbd6ea8e54fcd24f9efdef39979d37464
JIHYE <jdh324@naver.com>
2024-11-06 18:51:58 +0900

    ✨Feat: prisma 관련 module 추가
    - prisma service는 Global로 선언
    - balance module에는 @Transactional을 사용할 수 있도록 transaction 관련 설정 추가

--- BE/apps/balance/src/balance.module.ts
+++ BE/apps/balance/src/balance.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { BalanceController } from './balance.controller';
 import { BalanceService } from './balance.service';
+import { PrismaModule } from './databases/prisma/prisma.module';
+import { ClsModule } from 'nestjs-cls';
+import { PrismaService } from './databases/prisma/prisma.service';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ClsModule.forRoot({
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [PrismaModule],
+          adapter: new TransactionalAdapterPrisma({
+            prismaInjectionToken: PrismaService,
+          }),
+        }),
+      ],
+    }),
+  ],
   controllers: [BalanceController],
   providers: [BalanceService],
 })
