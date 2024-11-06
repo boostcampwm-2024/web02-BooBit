0aeeaf97bf2e848b99d54f5a20a12328e56f8484
pkh0106 <rudgml742@gmail.com>
2024-11-06 17:49:33 +0900

    ✨Feat. logo, SubmitButton 배치 #5

--- FE/src/pages/SignIn/index.tsx
+++ FE/src/pages/SignIn/index.tsx
@@ -1,7 +1,14 @@
 import AuthLayout from '../../widgets/AuthLayout';
+import logo from '../../shared/images/BuBuWithLogo.png';
+import SubmitButton from '../../shared/UI/SubmitButton';
 
 const SignIn = () => {
-  return <AuthLayout>로그인</AuthLayout>;
+  return (
+    <AuthLayout>
+      <img src={logo} alt="logo" className="w-[13rem]" />
+      <SubmitButton content="로그인" onClick={() => alert('로그인')} />
+    </AuthLayout>
+  );
 };
 
 export default SignIn;
--- FE/src/widgets/AuthLayout/index.tsx
+++ FE/src/widgets/AuthLayout/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { LayoutProps } from '../../shared/types/LayoutProps';
 
 const AuthLayout: React.FC<LayoutProps> = ({ children }) => {
-  return <div className="w-screen h-screen px-[36vw]">{children}</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center px-[38vw]">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
