4bda441ba878c2555e5418d42ee84cb9e911f54d
pkh0106 <rudgml742@gmail.com>
2024-11-06 18:19:34 +0900

    ✨Feat. custom input 컴포넌트 구현 #5

--- FE/src/pages/SignIn/index.tsx
+++ FE/src/pages/SignIn/index.tsx
@@ -2,9 +2,24 @@ import AuthLayout from '../../widgets/AuthLayout';
 import logo from '../../shared/images/BuBuWithLogo.png';
 import SubmitButton from '../../shared/UI/SubmitButton';
 import { useNavigate } from 'react-router-dom';
+import { useState, ChangeEvent } from 'react';
+import InputField from '../../shared/UI/InputFeild';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSumbit = () => {
+    alert('이메일: ' + email + '\n비밀번호: ' + password);
+  };
 
   const moveToSignup = () => {
     navigate('/signup');
@@ -12,9 +27,19 @@ const SignIn = () => {
 
   return (
     <AuthLayout>
-      <img src={logo} alt="logo" className="w-[12rem]" />
-      <SubmitButton content="로그인" onClick={() => alert('로그인')} />
-      <button className="ml-[52%] text-available-medium-16 text-text-dark" onClick={moveToSignup}>
+      <img src={logo} alt="logo" className="w-[11rem] mb-[2rem]" />
+      <InputField type="text" placeholder="이메일" value={email} onChange={handleEmailChange} />
+      <InputField
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <SubmitButton content="로그인" onClick={handleSumbit} />
+      <button
+        className="ml-[52%] mb-[5rem] text-available-medium-16 text-text-dark"
+        onClick={moveToSignup}
+      >
         아직 계정이 없다면? 회원가입하기
       </button>
     </AuthLayout>
---
+++ FE/src/shared/UI/InputFeild.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent } from 'react';
+
+interface InputFieldProps {
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ type, placeholder, value, onChange }) => {
+  return (
+    <input
+      className="w-[100%] h-[3.5rem] px-[2rem] mb-[1rem] border-[1px] border-border-default rounded-[5px] text-available-medium-16 text-text-light bg-surface-default"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default InputField;
