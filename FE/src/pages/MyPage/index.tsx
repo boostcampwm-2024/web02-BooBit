138b3f4014dc251e774784a88028cac5316aa92f
pkh0106 <rudgml742@gmail.com>
2024-11-06 12:33:48 +0900

    ✨Feat. 세로선 컴포넌트 구현 #44

--- FE/src/pages/MyPage/index.tsx
+++ FE/src/pages/MyPage/index.tsx
@@ -1,11 +1,15 @@
 import Header from '../../widgets/Header';
 import Layout from '../../widgets/Layout';
+import VerticalDivider from './UI/VerticalDivider';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Layout paddingX="px-[16rem]">마이페이지</Layout>
+      <Layout paddingX="px-[14vw]">
+        마이페이지
+        <VerticalDivider />
+      </Layout>
     </div>
   );
 };
--- FE/src/widgets/Layout/index.tsx
+++ FE/src/widgets/Layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { LayoutProps } from '../../shared/types/LayoutProps';
 
 const Layout: React.FC<LayoutProps> = ({ children, paddingX = 'px-[15rem]' }) => {
-  return <div className={`w-screen min-h-64 ${paddingX} text-text-light`}>{children}</div>;
+  return <div className={`w-screen min-h-64 flex ${paddingX} text-text-light`}>{children}</div>;
 };
 
 export default Layout;
