7d0737f0e9e64187071115e5f1aca369da48fdaf
pkh0106 <rudgml742@gmail.com>
2024-11-07 10:48:09 +0900

    ✨Feat. form 데이터 에러 시 빨간 테두리와 안내 컴포넌트 적용 #14

--- FE/src/shared/UI/InputFeild.tsx
+++ FE/src/shared/UI/InputFeild.tsx
@@ -6,18 +6,35 @@ interface InputFieldProps {
   placeholder: string;
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  isError?: boolean;
+  errorMessage?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = ({ type, name, placeholder, value, onChange }) => {
+const InputField: React.FC<InputFieldProps> = ({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  isError = false,
+  errorMessage = '',
+}) => {
   return (
-    <input
-      className="w-[100%] h-[3.5rem] px-[2rem] mb-[1rem] border-[1px] border-border-default rounded-[5px] text-available-medium-16 text-text-light bg-surface-default"
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
+    <form className="w-[100%] mb-[1rem]">
+      <input
+        className={`w-[100%] h-[3.5rem] px-[2rem]   ${isError ? 'border-[2px] border-negative' : 'border-[1px] border-border-default'} rounded-[5px] text-available-medium-16 text-text-light bg-surface-default`}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {isError ? (
+        <label className="text-negative  text-available-medium-14">{errorMessage}</label>
+      ) : (
+        ''
+      )}
+    </form>
   );
 };
 
