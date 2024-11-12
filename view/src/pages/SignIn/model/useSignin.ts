22da008a4f1b7e6e006616757e6f47b7a4d9b2de
pkh0106 <rudgml742@gmail.com>
2024-11-12 16:55:25 +0900

    ✨Feat. 로그인 정보 전역 관리 #11

--- view/src/pages/SignIn/model/useSignin.ts
+++ view/src/pages/SignIn/model/useSignin.ts
@@ -1,14 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 import signinApi from '../api/signinApi';
 import { useNavigate } from 'react-router-dom';
+import { useAuthActions } from '../../../shared/store/auth/authActions';
 
 const useSignin = () => {
   const navigate = useNavigate();
+  const { login } = useAuthActions();
   return useMutation({
     mutationFn: signinApi,
     onSuccess: () => {
       alert('로그인 성공:');
       navigate('/');
+      login();
     },
     onError: (error: unknown) => {
       if (error instanceof Error) {
