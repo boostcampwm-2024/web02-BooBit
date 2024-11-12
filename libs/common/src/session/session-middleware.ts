abfae220aa421366d60756ab9616a8c60c58d3ab
tongticktack <provemyself99@gmail.com>
2024-11-12 10:51:24 +0900

    ♻️Refactor. 세션 관리 코드 common으로 이동
    - 배포 시 고려할 사항
    1. redis 주소
    2. 동일한 SECRETE_KEY

---
+++ libs/common/src/session/session-middleware.ts
@@ -0,0 +1,37 @@
+import session from 'express-session';
+import { createClient } from 'redis';
+import RedisStore from 'connect-redis';
+import * as crypto from 'crypto';
+import passport from 'passport';
+
+export const createSessionConfig = async () => {
+  const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    legacyMode: false,
+  });
+
+  await redisClient.connect().catch(console.error);
+
+  const sessionMiddleware = session({
+    store: new RedisStore({
+      client: redisClient,
+      prefix: 'sid:',
+    }),
+    secret: process.env.SESSION_SECRET || crypto.randomBytes(32).toString('hex'),
+    resave: false,
+    saveUninitialized: false,
+    name: 'sid',
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      sameSite: 'strict',
+    },
+  });
+
+  return {
+    session: sessionMiddleware,
+    passportInitialize: passport.initialize(),
+    passportSession: passport.session(),
+    redisClient,
+  };
+};
