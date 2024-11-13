b0685b8a5be50fde427ff1dbcdd900022a1200ac
I`m Taerae <dkfhdksp@gmail.com>
2024-11-13 12:49:54 +0900

    🐛Fix. 깜빡 놓친 밸런스모듈 main.ts fix

--- apps/balance/src/main.ts
+++ apps/balance/src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { BalanceModule } from './balance.module';
 import { PrismaService } from '@app/prisma';
-import { createSessionConfig } from '@app/common/session/session-middleware';
+
 async function bootstrap() {
   const app = await NestFactory.create(BalanceModule);
   const prismaService = app.get(PrismaService);
-
-  const { session, passportInitialize, passportSession } = await createSessionConfig();
-  app.use(session);
-  app.use(passportInitialize);
-  app.use(passportSession);
 
   await prismaService.enableShutdownHooks();
   app.enableCors({
