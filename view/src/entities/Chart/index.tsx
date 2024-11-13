090c37a220751aaa7cdb9e1baa9d9be069d8385a
pkh0106 <rudgml742@gmail.com>
2024-11-13 19:04:54 +0900

    🛠️Update: 차트 관련 변수 명 변경 #60

--- view/src/entities/Chart/index.tsx
+++ view/src/entities/Chart/index.tsx
@@ -23,12 +23,12 @@ const Chart: React.FC<CandleChartProps> = ({ data }) => {
     const margin = { top: 20, right: 90, bottom: 50, left: 40 };
     const width = 1076 - margin.left - margin.right;
     const height = 460 - margin.top - margin.bottom;
-    const volumnHeight = 80;
-
+    const volumeHeight = 80;
     const svg = d3.select(svgRef.current);
+
     svg.selectAll('*').remove();
 
-    //배경 그리기
+    // Setup main group for chart
     const mainGroup = svg
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
@@ -36,8 +36,8 @@ const Chart: React.FC<CandleChartProps> = ({ data }) => {
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // X축과 Y축 스케일 설정
-    const x = d3
+    // X and Y axis scales
+    const xScale = d3
       .scaleBand()
       .domain(data.map((d) => d.date.toISOString()))
       .range([0, width])
@@ -45,32 +45,28 @@ const Chart: React.FC<CandleChartProps> = ({ data }) => {
 
     const yMin = d3.min(data, (d) => d.low)!;
     const yMax = d3.max(data, (d) => d.high)!;
-    // 여백 비율 설정 (예: 5%)
-    const paddingRatio = 0.02;
-    const y = d3
+    const paddingRatio = 0.0;
+
+    const yScale = d3
       .scaleLinear()
       .domain([yMin - (yMax - yMin) * paddingRatio, yMax + (yMax - yMin) * paddingRatio])
       .nice()
-      .range([height - volumnHeight, 0]);
+      .range([height - volumeHeight, 0]);
 
-    // x축 y축
-    const xAxisG = d3
-      .select(svgRef.current)
+    // Axes
+    const xAxisG = svg
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top + height})`);
-    const yAxisG = d3
-      .select(svgRef.current)
+    const yAxisG = svg
       .append('g')
       .attr('transform', `translate(${margin.left + width}, ${margin.top})`);
 
     const xAxis = d3
-      .axisBottom(x)
+      .axisBottom(xScale)
       .tickValues(
         data
           .map((d) => {
-            const date = d.date;
-            const day = date.getDate();
-            return day === 1 || day === 16 ? date.toISOString() : null;
+            return d.date.getDate() === 1 || d.date.getDate() === 16 ? d.date.toISOString() : null;
           })
           .filter((d) => d !== null)
       )
@@ -80,77 +76,73 @@ const Chart: React.FC<CandleChartProps> = ({ data }) => {
         return day === 1 ? d3.timeFormat('%b')(date) : '16';
       });
 
-    const yAxis = d3.axisRight(y).ticks(5);
-
+    const yAxis = d3.axisRight(yScale).ticks(5);
     xAxisG.call(xAxis);
     yAxisG.call(yAxis);
 
-    // 고점과 저점 선 그리기
+    // Candlestick lines
     mainGroup
       .selectAll('.line')
       .data(data)
       .enter()
       .append('line')
-      .attr('x1', (d) => x(d.date.toISOString())! + x.bandwidth() / 2)
-      .attr('x2', (d) => x(d.date.toISOString())! + x.bandwidth() / 2)
-      .attr('y1', (d) => y(d.high))
-      .attr('y2', (d) => y(d.low))
+      .attr('x1', (d) => xScale(d.date.toISOString())! + xScale.bandwidth() / 2)
+      .attr('x2', (d) => xScale(d.date.toISOString())! + xScale.bandwidth() / 2)
+      .attr('y1', (d) => yScale(d.high))
+      .attr('y2', (d) => yScale(d.low))
       .attr('stroke', '#E0E0E0')
       .attr('stroke-width', 1);
 
-    // rect 그리기
+    // Candlestick rectangles
     mainGroup
       .append('g')
       .selectAll('.bar')
       .data(data)
       .enter()
       .append('rect')
-      .attr('x', (d) => x(d.date.toISOString())!)
-      .attr('y', (d) => y(Math.max(d.open, d.close)))
-      .attr('width', x.bandwidth())
-      .attr('height', (d) => Math.abs(y(d.open) - y(d.close)))
+      .attr('x', (d) => xScale(d.date.toISOString())!)
+      .attr('y', (d) => yScale(Math.max(d.open, d.close)))
+      .attr('width', xScale.bandwidth())
+      .attr('height', (d) => Math.abs(yScale(d.open) - yScale(d.close)))
       .attr('fill', (d) => (d.open > d.close ? '#FF5252' : '#00E676'));
 
-    // # 구분선
+    // Divider line
     mainGroup
       .append('line')
       .attr('x1', 0)
       .attr('x2', width)
-      .attr('y1', height - volumnHeight)
-      .attr('y2', height - volumnHeight)
+      .attr('y1', height - volumeHeight)
+      .attr('y2', height - volumeHeight)
       .attr('stroke', '#A0A0A0')
       .attr('stroke-width', 0.5);
 
-    // # 거래량 그래프
-
+    // Volume chart
     const volumeMax = d3.max(data, (d) => d.volume)!;
-    const yVolumn = d3
+    const yVolumeScale = d3
       .scaleLinear()
-      .domain([0, volumeMax + volumeMax * 0.2])
-      .range([volumnHeight, 0]);
+      .domain([0, volumeMax * 1.2])
+      .range([volumeHeight, 0]);
 
-    const yVolumeAxisG = d3
-      .select(svgRef.current)
+    const yVolumeAxisG = svg
       .append('g')
       .attr(
         'transform',
-        `translate(${margin.left + width}, ${margin.top + height - volumnHeight})`
+        `translate(${margin.left + width}, ${margin.top + height - volumeHeight})`
       );
 
-    const yVolumeAxis = d3.axisRight(yVolumn).ticks(3);
-
+    const yVolumeAxis = d3.axisRight(yVolumeScale).ticks(3);
     yVolumeAxisG.call(yVolumeAxis);
 
     mainGroup
       .append('g')
-      .selectAll('.volumn-bar')
+      .selectAll('.volume-bar')
       .data(data)
       .enter()
       .append('rect')
-      .attr('x', (d) => x(d.date.toISOString())!)
-      .attr('y', (d) => height - volumnHeight + yVolumn(d.volume))
-      .attr('width', x.bandwidth())
-      .attr('height', (d) => volumnHeight - yVolumn(d.volume))
+      .attr('x', (d) => xScale(d.date.toISOString())!)
+      .attr('y', (d) => height - volumeHeight + yVolumeScale(d.volume))
+      .attr('width', xScale.bandwidth())
+      .attr('height', (d) => volumeHeight - yVolumeScale(d.volume))
       .attr('fill', (d) => (d.open > d.close ? '#FF5252' : '#00E676'));
   }, [data]);
 
