65d5249f1c30d616660c7dc4de989beb162ced65
pkh0106 <rudgml742@gmail.com>
2024-11-14 14:31:19 +0900

    ✨Feat. 상단 시간 scale 변경 컴포넌트 page에 적용 #61

--- view/src/pages/Home/index.tsx
+++ view/src/pages/Home/index.tsx
@@ -4,17 +4,23 @@ import Header from '../../widgets/Header';
 import Layout from '../../widgets/Layout';
 import candleMockData from './consts/candleData';
 import generateCandleData from './lib/generateCandleData';
+import TimeScaleSelector from './UI/TimeScaleSelector';
 
 const Home = () => {
   const [candleData, setCandleData] = useState(candleMockData);
+  const [selectedTimeScale, setSelectedTimeScale] = useState('1d');
 
   const handleButtonClick = () => {
-    setCandleData((prevData) => generateCandleData(prevData)); // 버튼 클릭 시 데이터 업데이트
+    setCandleData((prevData) => generateCandleData(prevData));
   };
   return (
     <div>
       <Header />
       <Layout paddingX="px-[22vw]" flex={false}>
+        <TimeScaleSelector
+          selectedTimeScale={selectedTimeScale}
+          setSelectedTimeScale={setSelectedTimeScale}
+        />
         <Chart data={candleData} />
         <button onClick={handleButtonClick}>Generate More Data</button>
       </Layout>
