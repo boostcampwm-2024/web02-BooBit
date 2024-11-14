82b33aef230c93581eebef006169b03a8e31a887
tongticktack <provemyself99@gmail.com>
2024-11-14 17:50:47 +0900

    🐛Fix. 변수명 및 타입 변경, 테스트 쿼리 삭제
    - OrderLimitRequestDto: 지정가 매수, 매도 요청
      - coin_code -> coinCode
    - order.proto 필드명 변경
      - orderId -> historyId

--- apps/transaction/src/dto/order.limit.request.dto.ts
+++ apps/transaction/src/dto/order.limit.request.dto.ts
@@ -1,9 +1,7 @@
 import { IsNotEmpty, IsNumber, IsEnum } from 'class-validator';
-import { Expose } from 'class-transformer';
 import { CurrencyCode } from '@app/common';
 
 export class OrderLimitRequestDto {
-  @Expose({ name: 'coin_code' })
   @IsEnum(CurrencyCode)
   @IsNotEmpty()
   coinCode: string;
