460293d0dcb0507db05ff27373d15303913a466e
pkh0106 <rudgml742@gmail.com>
2024-11-16 13:20:47 +0900

    ⚡️Improve: 에러 핸들링 전역 처리 #4

--- view/src/entities/MyAssetInfo/model/useWithdraw.ts
+++ view/src/entities/MyAssetInfo/model/useWithdraw.ts
@@ -1,12 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
-import { useAuthActions } from '../../../shared/store/auth/authActions';
 import withdrawApi from '../api/withdrawApi';
 
 const useWithdraw = () => {
-  const navigate = useNavigate();
-  const { logout } = useAuthActions();
-
   return useMutation({
     mutationFn: withdrawApi,
     onSuccess: () => {
@@ -14,12 +9,7 @@ const useWithdraw = () => {
     },
     onError: (error: unknown) => {
       if (error instanceof Error) {
-        if (error.message === '403') {
-          logout();
-          navigate('/signin');
-        } else {
-          alert('출금에 실패했습니다. 다시 시도해주세요.');
-        }
+        alert('출금에 실패했습니다. 다시 시도해주세요.');
       } else {
         alert('출금에 실패했습니다. 다시 시도해주세요.');
       }
