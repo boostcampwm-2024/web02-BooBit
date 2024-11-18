7e91b02699d4972c27bbc2a5b0d6677d7e925c56
pkh0106 <rudgml742@gmail.com>
2024-11-18 17:41:05 +0900

    🐛Fix. 가로 스크롤 생성 문제 해결 #94

--- view/src/widgets/Layout/index.tsx
+++ view/src/widgets/Layout/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { LayoutProps } from '../../shared/types/LayoutProps';
 
-const Layout: React.FC<LayoutProps> = ({ children, paddingX = 'px-[15rem]', flex = true }) => {
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  paddingX = 'px-[15rem]',
+  flex = true,
+}) => {
   return (
-    <div className={`w-screen min-h-64 ${flex ? 'flex' : ''} ${paddingX} text-text-light`}>
+    <div
+      className={`w-full min-h-64 ${flex ? 'flex' : ''} ${paddingX} text-text-light`}
+    >
       {children}
     </div>
   );
