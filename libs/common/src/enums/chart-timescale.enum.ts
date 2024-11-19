c3eb3e007e882dc8942e7a23381caf41e4e417dc
tongticktack <provemyself99@gmail.com>
2024-11-19 14:39:15 +0900

    🐛Fix. timeScale Enum 수정

--- libs/common/src/enums/chart-timescale.enum.ts
+++ libs/common/src/enums/chart-timescale.enum.ts
@@ -1,9 +1,10 @@
 export enum TimeScale {
   SEC_01 = '1sec',
   MIN_01 = '1min',
-  MIN_15 = '15min',
+  MIN_10 = '10min',
   MIN_30 = '30min',
   HOUR_01 = '1hour',
   DAY_01 = '1day',
-  YEAR_01 = '1year',
+  WEEK_01 = '1week',
+  MONTH_01 = '1month',
 }
