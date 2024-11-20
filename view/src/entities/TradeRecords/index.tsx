30b5aa4d5a1dd1f16e53949663759894f6f3f7c7
pkh0106 <rudgml742@gmail.com>
2024-11-20 11:25:45 +0900

    🛠️Update: 체결 목록 내부 컴포넌트 모듈화 #101
    - tr ->TradeRecordsRow
    - td ->TradeRecordsCell

--- view/src/entities/TradeRecords/index.tsx
+++ view/src/entities/TradeRecords/index.tsx
@@ -1,3 +1,6 @@
+import TradeRecordsRow from './UI/TradeRecordRow';
+import TradeRecordsCell from './UI/TradeRecordsCell';
+
 interface Record {
   date: string;
   price: number;
@@ -12,38 +15,35 @@ interface TradeRecordsProps {
 
 const TradeRecords: React.FC<TradeRecordsProps> = ({ tradeRecords }) => {
   return (
-    <table className="w-full h-[30rem]  mb-12 border-[1px] bg-surface-default border-border-default text-text-light">
+    <table className="w-full h-[30rem] mb-12 border-[1px] bg-surface-default border-border-default text-text-light">
       <thead>
-        <tr className="w-full h-[2.5rem] ">
-          <td colSpan={4} className="text-center text-display-bold-20">
+        <TradeRecordsRow>
+          <td colSpan={4} className="text-display-bold-20">
             체결
           </td>
-        </tr>
-        <tr className="h-[2.5rem] flex justify-between items-center px-[2rem] bg-surface-hover-light text-center">
-          <td className="w-[12rem] text-start">체결시간</td>
-          <td className="w-[15rem]">체결가격(KRW)</td>
-          <td className="w-[15rem]">체결량(BTC)</td>
-          <td className="w-[15rem] text-end">체결금액(KRW)</td>
-        </tr>
+        </TradeRecordsRow>
+        <TradeRecordsRow flex styles="bg-surface-hover-light">
+          <TradeRecordsCell start>체결시간</TradeRecordsCell>
+          <TradeRecordsCell>체결가격(KRW)</TradeRecordsCell>
+          <TradeRecordsCell>체결량(BTC)</TradeRecordsCell>
+          <TradeRecordsCell end>체결금액(KRW)</TradeRecordsCell>
+        </TradeRecordsRow>
       </thead>
       <tbody className="block max-h-[25rem] overflow-y-auto">
         {tradeRecords.map((r) => (
-          <tr
-            key={r.amount + '.' + r.date}
-            className="h-[2.5rem] flex justify-between items-center px-[2rem] even:bg-surface-hover-light text-center "
-          >
-            <td className="w-[12rem] text-start">
+          <TradeRecordsRow key={r.amount + '.' + r.date} flex styles="even:bg-surface-hover-light">
+            <TradeRecordsCell start>
               <span>{r.date.slice(0, 4)}</span>
               <span className="text-text-dark">{r.date.slice(5)}</span>
-            </td>
-            <td className="w-[15rem]">{r.price.toLocaleString()}</td>
-            <td
-              className={`w-[15rem] ${r.gradient === 'POSITIVE' ? 'text-positive' : 'text-negative'}`}
+            </TradeRecordsCell>
+            <TradeRecordsCell>{r.price.toLocaleString()}</TradeRecordsCell>
+            <TradeRecordsCell
+              styles={r.gradient === 'POSITIVE' ? 'text-positive' : 'text-negative'}
             >
               {r.amount}
-            </td>
-            <td className="w-[15rem] text-end">{r.tradePrice.toLocaleString()}</td>
-          </tr>
+            </TradeRecordsCell>
+            <TradeRecordsCell end>{r.tradePrice.toLocaleString()}</TradeRecordsCell>
+          </TradeRecordsRow>
         ))}
       </tbody>
     </table>
