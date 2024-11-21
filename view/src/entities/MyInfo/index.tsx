7e4b634aa9c8b335e1f4c6eb0b817b79868998ae
pkh0106 <rudgml742@gmail.com>
2024-11-21 12:14:10 +0900

    ✨Feat: 로그인 API 연결 #134
    - 데이터가 없으면 스켈레톤 컴포넌트 출력
    - 403 에러면 로그인으로 이동

--- view/src/entities/MyInfo/index.tsx
+++ view/src/entities/MyInfo/index.tsx
@@ -1,13 +1,15 @@
-import myInfoMockData from './const/myInfoMockData';
+import useGetProfile from './model/useGetProfile';
 import InfoItem from './UI/InfoItem';
 
 const MyInfo = () => {
-  const myInfo = myInfoMockData;
-  return (
+  const { data: myInfo } = useGetProfile();
+  return myInfo ? (
     <div className="w-full h-[19rem] px-[4rem] py-[2.5rem] rounded bg-surface-default border-border-default border-[1px]">
       <InfoItem label="이름" value={myInfo.name} />
       <InfoItem label="이메일" value={myInfo.email} />
     </div>
+  ) : (
+    <div className="w-full h-[19rem] px-[4rem] py-[2.5rem] rounded bg-surface-default border-border-default border-[1px]"></div>
   );
 };
 
