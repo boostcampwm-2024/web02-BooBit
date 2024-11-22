a359e3e8cd7e91de9a7c3aa6ff5715250f56c0bf
JIHYE <jdh324@naver.com>
2024-11-23 06:21:27 +0900

    ✨Feat. 체결 processor 구현

---
+++ microservice/apps/trade/src/trade.processor.ts
@@ -0,0 +1,41 @@
+import { Processor, WorkerHost, OnWorkerEvent } from '@nestjs/bullmq';
+import { OrderType } from '@app/common/enums/order-type.enum';
+import { Logger } from '@nestjs/common';
+import { Job } from 'bullmq';
+import { TradeService } from './trade.service';
+
+@Processor('trade', { concurrency: 1 })
+export class TradeProcessor extends WorkerHost {
+  private readonly logger = new Logger(TradeProcessor.name);
+
+  constructor(private tradeService: TradeService) {
+    super();
+  }
+
+  async process(job: Job<any>): Promise<void> {
+    this.logger.log(`Processing job: ${job.id}`);
+
+    switch (job.name) {
+      case OrderType.BUY:
+        await this.tradeService.tradeBuyOrder(job.id);
+        break;
+
+      case OrderType.SELL:
+        await this.tradeService.tradeSellOrder(job.id);
+        break;
+
+      default:
+        this.logger.error(`Unknown job type: ${job.name}`);
+    }
+  }
+
+  @OnWorkerEvent('completed')
+  onCompleted(job: Job) {
+    this.logger.log(`Job completed: ${job.id}`);
+  }
+
+  @OnWorkerEvent('failed')
+  onFailed(job: Job, error: Error) {
+    this.logger.error(`Job failed: ${job.id}`, error);
+  }
+}
