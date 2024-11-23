1987cc9829bb99008daa4f6315aa55537c069751
tongticktack <provemyself99@gmail.com>
2024-11-23 23:09:54 +0900

    ✨Feat. 초기 연결 데이터 Repo
    - 체결 데이터
    - timeScale 캔들 차트 데이터

--- microservice/apps/transaction/src/transaction.repository.ts
+++ microservice/apps/transaction/src/transaction.repository.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@app/prisma';
 import { OrderRequestDto } from '@app/grpc/dto/order.request.dto';
+import { TimeScale } from '@app/common/enums/chart-timescale.enum';
 
 @Injectable()
 export class TransactionRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getLatestCandles(tableName: string, count: number) {
+    const candles = await this.prisma[tableName].findMany({
+      take: count,
+      orderBy: {
+        startTime: 'desc',
+      },
+      select: {
+        startTime: true,
+        openPrice: true,
+        closePrice: true,
+        highPrice: true,
+        lowPrice: true,
+        volume: true,
+      },
+    });
+
+    return candles;
+  }
+
+  async getLatestTrades(count: number = 20) {
+    const trades = await this.prisma.trade.findMany({
+      take: count,
+      orderBy: {
+        tradedAt: 'desc',
+      },
+      select: {
+        tradedAt: true,
+        price: true,
+        quantity: true,
+      },
+    });
+
+    return trades;
+  }
 
   async createBuyOrder(orderRequest: OrderRequestDto, historyId: string) {
     return await this.prisma.buyOrder.create({
