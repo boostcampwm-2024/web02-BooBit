7856232d3140385baedcd1953cdab2f206d3aec5
pkh0106 <rudgml742@gmail.com>
2024-11-24 20:50:09 +0900

    ♻️Refactor. mocks 파일 명 변경

--- view/src/app/main.tsx
+++ view/src/app/main.tsx
@@ -9,7 +9,7 @@ import { ToastProvider } from '../shared/store/ToastContext';
 async function enableMocking() {
   if (import.meta.env.MODE !== 'development') return;
 
-  const { worker } = await import('../mocks/browser');
+  const { worker } = await import('../__mocks/browser');
 
   return worker.start();
 }
