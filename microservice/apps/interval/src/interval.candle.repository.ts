1be826ea87e42fe28e7acdc8b01282323a6f63d5
I`m Taerae <105408841+TaeRaeKim@users.noreply.github.com>
2024-11-24 19:32:45 +0900

    ✨Feat. candle 데이터 저장 및 select을 위한 repository 구현

---
+++ microservice/apps/interval/src/interval.candle.repository.ts
@@ -0,0 +1,111 @@
+import { CurrencyCode } from '@app/common';
+import { TimeScale } from '@app/common/enums/chart-timescale.enum';
+import { CandleDataDto } from '@app/ws/dto/candle.data.dto';
+import { Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+export class IntervalRepository {
+  private prisma: PrismaClient;
+  private logger = new Logger(IntervalRepository.name);
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  private getModelForInterval(interval: TimeScale) {
+    const modelMap = {
+      '1sec': this.prisma.candle01Sec,
+      '1min': this.prisma.candle01Min,
+      '10min': this.prisma.candle10Min,
+      '30min': this.prisma.candle30Min,
+      '1hour': this.prisma.candle01Hour,
+      '1day': this.prisma.candle01Day,
+      '1week': this.prisma.candle01Week,
+      '1month': this.prisma.candle01Month,
+    };
+    return modelMap[interval];
+  }
+  async getLatestTrade(coinCode?: string) {
+    try {
+      const trade = await this.prisma.trade.findFirst({
+        where: {
+          ...(coinCode && { coinCode }),
+        },
+        orderBy: {
+          tradedAt: 'desc', // 날짜 기준 내림차순 정렬
+        },
+      });
+
+      return trade;
+    } catch (error) {
+      console.error('최근 거래 내역 조회 중 오류 발생:', error);
+      throw error;
+    }
+  }
+
+  async getTradesByDateRange(startDate: Date, endDate: Date, coinCode?: string) {
+    try {
+      const trades = await this.prisma.trade.findMany({
+        where: {
+          tradedAt: {
+            gte: startDate,
+            lt: endDate,
+          },
+          ...(coinCode && { coinCode }),
+        },
+        orderBy: {
+          tradedAt: 'desc',
+        },
+      });
+
+      return trades;
+    } catch (error) {
+      console.error('거래 내역 조회 중 오류 발생:', error);
+      throw error;
+    }
+  }
+
+  async saveCandle(interval: TimeScale, data: CandleDataDto) {
+    const model = this.getModelForInterval(interval);
+
+    try {
+      return await model.create({
+        data: {
+          coinCode: CurrencyCode.BTC,
+          startTime: data.date,
+          openPrice: data.open.toString(),
+          highPrice: data.high.toString(),
+          lowPrice: data.low.toString(),
+          closePrice: data.close.toString(),
+          volume: data.volume.toString(),
+        },
+      });
+    } catch (error) {
+      this.logger.error('Error in saveCandle', error);
+    }
+  }
+
+  async getCandles(
+    interval: TimeScale,
+    coinCode: string,
+    startTime: Date,
+    endTime: Date,
+    limit?: number,
+  ) {
+    const model = this.getModelForInterval(interval);
+
+    return await (model as any).findMany({
+      where: {
+        coinCode,
+        startTime: {
+          gte: startTime,
+          lt: endTime,
+        },
+      },
+      orderBy: {
+        startTime: 'desc',
+      },
+      take: limit,
+    });
+  }
+}
