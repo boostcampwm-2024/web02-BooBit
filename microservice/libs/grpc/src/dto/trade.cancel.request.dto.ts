9fae572920155a2548a3ebd7d37be50801f00176
JIHYE <jdh324@naver.com>
2024-11-25 16:27:36 +0900

    ♻️Refactor. orderHistory 테이블 사용이 변경됨에 따라 orderStatus를 ORDERED, CANCELED로 변경

--- microservice/libs/grpc/src/dto/trade.cancel.request.dto.ts
+++ microservice/libs/grpc/src/dto/trade.cancel.request.dto.ts
@@ -1,27 +1,21 @@
 export class TradeCancelRequestDto {
   userId: string;
-  historyId: string;
   coinCode: string;
   price: string;
   remain: string;
   orderType: string;
-  orderStatus: string;
 
   constructor(
     userId: string,
-    historyId: string,
     coinCode: string,
     price: string,
     remain: string,
     orderType: string,
-    orderStatus: string,
   ) {
     this.userId = userId;
-    this.historyId = historyId;
     this.coinCode = coinCode;
     this.price = price;
     this.remain = remain;
     this.orderType = orderType;
-    this.orderStatus = orderStatus;
   }
 }
--- microservice/libs/grpc/src/dto/trade.history.request.dto.ts
+++ microservice/libs/grpc/src/dto/trade.history.request.dto.ts
@@ -1,13 +1,9 @@
-import { OrderStatus } from '@app/common/enums/order-status.enum';
-
 export class TradeHistoryRequestDto {
   historyId: string;
-  status: OrderStatus;
   remain: number;
 
-  constructor(historyId: string, status: OrderStatus, remain: number) {
+  constructor(historyId: string, remain: number) {
     this.historyId = historyId;
-    this.status = status;
     this.remain = remain;
   }
 }
