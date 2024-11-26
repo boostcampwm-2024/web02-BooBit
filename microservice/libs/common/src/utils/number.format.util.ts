34c665d9966665f3698604148232e3db9a1f1355
tongticktack <provemyself99@gmail.com>
2024-11-26 14:05:13 +0900

    🔥Hotfix. util 소수점 6자리까지로 변환 메서드

--- microservice/libs/common/src/utils/number.format.util.ts
+++ microservice/libs/common/src/utils/number.format.util.ts
@@ -3,3 +3,7 @@ export function formatFixedPoint(value: number, paddingLength = 15): string {
   const paddedInteger = integerPart.padStart(paddingLength, '0');
   return decimalPart ? `${paddedInteger}.${decimalPart}` : paddedInteger;
 }
+
+export const roundToSix = (num: number): number => {
+  return Number(num.toFixed(6));
+};
