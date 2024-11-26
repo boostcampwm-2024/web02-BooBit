2eddd230426847f8b75b4e79e83b48709931f9de
tongticktack <provemyself99@gmail.com>
2024-11-26 11:59:27 +0900

    🔥Hotfix. 또 놓쳤다. 카멜 케이스..
    - 주문가능금액 API DTO 스네이크 -> 카멜

--- microservice/apps/balance/src/dto/available.balance.response.dto.ts
+++ microservice/apps/balance/src/dto/available.balance.response.dto.ts
@@ -1,9 +1,9 @@
 export class AvailableBalanceResponseDto {
-  available_balance: number;
-  currency_code: string;
+  availableBalance: number;
+  currencyCode: string;
 
   constructor(availableBalance: number, currencyCode: string) {
-    this.available_balance = availableBalance;
-    this.currency_code = currencyCode;
+    this.availableBalance = availableBalance;
+    this.currencyCode = currencyCode;
   }
 }
