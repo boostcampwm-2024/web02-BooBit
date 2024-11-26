83ff17b87fe21efee601074f32149b7fe2d93fb3
pkh0106 <rudgml742@gmail.com>
2024-11-26 11:12:46 +0900

    ♻️Refactor. 소켓 msw 참조 제거

--- view/src/__mocks/handlers/index.ts
+++ view/src/__mocks/handlers/index.ts
@@ -1,5 +1,4 @@
 import { transactionHandlers } from './transactionHandlers.ts';
 import { balanceHandlers } from './balanceHandlers.ts';
-import websocketHandlers from './websocketHandlers.ts';
 
-export const handlers = [...transactionHandlers, ...balanceHandlers, ...websocketHandlers];
+export const handlers = [...transactionHandlers, ...balanceHandlers];
