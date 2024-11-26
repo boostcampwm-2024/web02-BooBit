6213efa0ea5b02f68d6807021a399a3d7abc313c
pkh0106 <rudgml742@gmail.com>
2024-11-26 21:27:06 +0900

    🐛Fix. candle chart init 에러 해결
    렌더링과 웹 소켓 순서 문제
    date -> string 변환

--- view/src/shared/model/useWebSocket.ts
+++ view/src/shared/model/useWebSocket.ts
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import { WebSocketMessage } from '../types/socket/WebSocketMessageType';
 
-const useWebSocket = (url: string) => {
+const useWebSocket = (url: string, handleMessage: (message: WebSocketMessage) => void) => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [message, setMessage] = useState<WebSocketMessage | null>(null);
 
   useEffect(() => {
     const ws = new WebSocket(url);
@@ -11,7 +10,6 @@ const useWebSocket = (url: string) => {
 
     ws.onopen = () => {
       console.log('웹소켓 연결 완료');
-      // WebSocket 연결 후 CANDLE_CHART_INIT 요청 보내기 (예: "1min" timeScale)
       const initMessage = {
         event: 'CANDLE_CHART_INIT',
         timeScale: '1sec', // 원하는 시간 단위로 변경 가능
@@ -21,14 +19,13 @@ const useWebSocket = (url: string) => {
 
     ws.onmessage = (event) => {
       const receivedData = JSON.parse(event.data);
-      setMessage(receivedData); // 서버에서 받은 데이터를 상태에 저장
+      handleMessage(receivedData); // Pass the message to the parent component or handler directly
     };
 
-    // WebSocket 연결 종료 시
     return () => {
       ws.close();
     };
-  }, [url]);
+  }, [url, handleMessage]);
 
   // WebSocket에 메시지 보내기
   const sendMessage = (msg: string) => {
@@ -37,7 +34,7 @@ const useWebSocket = (url: string) => {
     }
   };
 
-  return { message, sendMessage };
+  return { sendMessage };
 };
 
 export default useWebSocket;
