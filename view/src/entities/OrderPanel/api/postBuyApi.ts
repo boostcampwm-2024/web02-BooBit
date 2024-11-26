a80359f5c2b530dd75436924abd0616bf27d729a
pkh0106 <rudgml742@gmail.com>
2024-11-26 12:54:56 +0900

    ♻️Refactor. baseurl env 설정

--- view/src/entities/OrderPanel/api/postBuyApi.ts
+++ view/src/entities/OrderPanel/api/postBuyApi.ts
@@ -1,8 +1,8 @@
-import { BASE_URLS } from '../../../shared/consts/baseUrl';
 import { RequestDataType } from '../model/RequestDataType';
 
+const apiUrl = import.meta.env.VITE_TRANSACTION_URL;
 const postBuyApi = async (requestData: RequestDataType) => {
-  const response = await fetch(`${BASE_URLS.TRANSACTION}/api/orders/limit/buy`, {
+  const response = await fetch(`${apiUrl}/api/orders/limit/buy`, {
     method: 'POST',
     credentials: 'include',
     headers: {
