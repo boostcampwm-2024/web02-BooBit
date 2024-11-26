77f39c0e4f557b09ae16909701ad12e7388c0a71
pkh0106 <rudgml742@gmail.com>
2024-11-26 14:09:26 +0900

    🐛Fix. 차트 scale 변경 시 체결 중복 데이터 발생 에러 해결
    - init 웹소켓 이벤트 발생 시 기존 체결 내역 초기화

--- view/src/pages/Home/index.tsx
+++ view/src/pages/Home/index.tsx
@@ -118,6 +118,7 @@ const Home = () => {
       timeScale: selectedTimeScale,
     };
     sendMessage(JSON.stringify(initMessage));
+    setTradeRecords([]);
   }, [selectedTimeScale]);
 
   return (
