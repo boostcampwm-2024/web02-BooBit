12895de321e84c16d512b78b1deb84c9c4c7c334
tongticktack <provemyself99@gmail.com>
2024-12-03 14:21:43 +0900

    🔥Hotfix. candle_data_init
    - 데이터 59 + 1 미래 dummy 데이터 반환

--- microservice/apps/transaction/src/gateway/transaction.ws.service.ts
+++ microservice/apps/transaction/src/gateway/transaction.ws.service.ts
@@ -13,8 +13,8 @@ export class TransactionWsService {
   }
 
   async getLatestCandles(timeScale: TimeScale, count: number = 60): Promise<CandleDataDto[]> {
-    const candles = await this.transactionRepository.getLatestCandles(timeScale, count);
-    return candles
+    const candles = await this.transactionRepository.getLatestCandles(timeScale, count - 1);
+    const latestCandles = candles
       .map((candle) => ({
         date: candle.startTime,
         open: parseFloat(candle.openPrice),
@@ -24,6 +24,48 @@ export class TransactionWsService {
         volume: parseFloat(candle.volume),
       }))
       .reverse();
+
+    const getNextCandleDate = (date: Date, timeScale: TimeScale): Date => {
+      const nextDate = new Date(date);
+      switch (timeScale) {
+        case TimeScale.SEC_01:
+          nextDate.setSeconds(nextDate.getSeconds() + 1);
+          break;
+        case TimeScale.MIN_01:
+          nextDate.setMinutes(nextDate.getMinutes() + 1);
+          break;
+        case TimeScale.MIN_10:
+          nextDate.setMinutes(nextDate.getMinutes() + 10);
+          break;
+        case TimeScale.MIN_30:
+          nextDate.setMinutes(nextDate.getMinutes() + 30);
+          break;
+        case TimeScale.HOUR_01:
+          nextDate.setHours(nextDate.getHours() + 1);
+          break;
+        case TimeScale.DAY_01:
+          nextDate.setDate(nextDate.getDate() + 1);
+          break;
+        case TimeScale.WEEK_01:
+          nextDate.setDate(nextDate.getDate() + 7);
+          break;
+        case TimeScale.MONTH_01:
+          nextDate.setMonth(nextDate.getMonth() + 1);
+          break;
+      }
+      return nextDate;
+    };
+
+    const lastDummyCandle = {
+      date: getNextCandleDate(latestCandles[latestCandles.length - 1].date, timeScale),
+      open: 0,
+      close: 0,
+      high: 0,
+      low: 0,
+      volume: 0,
+    };
+    latestCandles.push(lastDummyCandle);
+    return latestCandles;
   }
 
   async getLatestTrades(count: number = 20): Promise<TradeDataDto[]> {
